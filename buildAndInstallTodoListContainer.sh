616d7483be3e7b81ce386f577b1b5a163d2fbbae
Sebastien Errien <sebastien.errien@gmail.com>
2015-05-25 23:13:26 +0200

    Add samples commands for groups deploiyments, update and delete

--- buildAndInstallTodoListContainer.sh
+++ buildAndInstallTodoListContainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker build -t boune/todolist /vagrant/docker/todolist/
-sudo docker tag -f boune/todolist 192.168.33.201:5000/todolist
-sudo docker push 192.168.33.201:5000/todolist:latest
-sudo docker rmi boune/todolist
+sudo docker build -t boune/todolist:1.0.0 /vagrant/docker/todolist/
+sudo docker tag -f boune/todolist:1.0.0 192.168.33.201:5000/todolist:1.0.0
+sudo docker push 192.168.33.201:5000/todolist:1.0.0
+sudo docker rmi boune/todolist:1.0.0
